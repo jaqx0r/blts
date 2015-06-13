4376342fc99978e6f7c1c5475b59f31c3df2eefd
Jamie Wilkinson <jaq@spacepants.org>
2015-06-13 10:41:11 +1000

    Renice servers and add more load.

--- servers.sh
+++ servers.sh
@@ -6,6 +6,8 @@ trap "killall lb; killall s" HUP TERM INT EXIT
 
 export GOMAXPROCS=4
 
+renice +10 $$
+
 BACKENDS=""
 for i in $(seq 0 9); do
     (s/s --port 800$i 2>&1 | sed -e "s/^/s:800$i: /") &
