40abd68e3238c608b8dc93b1b7d3ead63da26f65
Jamie Wilkinson <jaq@spacepants.org>
2015-06-17 15:51:08 +1000

    New path for data.

--- prom.sh
+++ prom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf /tmp/metrics # Default local storage.
+rm -rf data # Default local storage.
 ~/src/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://localhost:9093
