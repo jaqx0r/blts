2e4521b8bdc2583c8883e6c9a52f6054a98a3f3b
Jamie Wilkinson <jaq@spacepants.org>
2015-06-18 10:05:55 +1000

    Increase total count of requests in the antagonist.

--- load.sh
+++ load.sh
@@ -2,7 +2,7 @@
 
 max=10
 for i in $(seq 1 $(( 2 * $max )) ); do
-    (ab -r -n 10000 -c 100 http://localhost:9001/hi 2>&1 | sed -e "s/^/$i: /") &
+    (ab -r -n 100000 -c 1000 http://localhost:9001/hi 2>&1 | sed -e "s/^/$i: /") &
     sleep $(echo $(( $max - $i )) | tr -d '-')
 done
 
