3ec9b0ce31477cc28e9f66e717d5fa42a153e476
Jamie Wilkinson <jaq@spacepants.org>
2016-02-01 23:37:11 +1100

    don't delete data

--- prom.sh
+++ prom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf data # Default local storage.
+#rm -rf data # Default local storage.
 ~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://localhost:9093
