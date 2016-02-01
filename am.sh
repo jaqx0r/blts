3dbe670195733ac7ea3b5ea5a11d88901bad977a
Jamie Wilkinson <jaq@spacepants.org>
2016-02-02 07:01:16 +1100

    hax

--- am.sh
+++ am.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/go/src/github.com/prometheus/alertmanager/alertmanager -config.file=prom/alertmanager.yml
+~/go/src/github.com/prometheus/alertmanager/alertmanager -config.file=prom/alertmanager.yml -storage.path=./am-data
--- prom.sh
+++ prom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #rm -rf data # Default local storage.
-~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://localhost:9093
+~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://localhost:9093 -storage.local.path=./prom-data
