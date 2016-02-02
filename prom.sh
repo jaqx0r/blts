d7157c582d89684153d7f81c68f6f292f2712464
Jamie Wilkinson <jaq@spacepants.org>
2016-02-02 11:01:44 +1100

    return url

--- prom.sh
+++ prom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #rm -rf data # Default local storage.
-~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://localhost:9093 -storage.local.path=./prom-data
+~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://127.0.0.1:9093 -storage.local.path=./prom-data -web.external-url=http://127.0.0.1:9090
