11f7ea68e40c60b030706df7f4eb4d4b3aba1498
Jamie Wilkinson <jaq@spacepants.org>
2016-02-02 11:01:30 +1100

    Return url

--- am.sh
+++ am.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/go/src/github.com/prometheus/alertmanager/alertmanager -config.file=prom/alertmanager.yml -storage.path=./am-data
+~/go/src/github.com/prometheus/alertmanager/alertmanager -config.file=prom/alertmanager.yml -storage.path=./am-data -web.listen-address=127.0.0.1:9093 -web.external-url=http://127.0.0.1:9093
