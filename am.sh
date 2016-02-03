b85cbc35e923d5cfb385bebddc49991bd6fbe715
Jamie Wilkinson <jaq@spacepants.org>
2016-02-03 11:47:18 +1100

    reformat am and proc scripts and raise GOMAXPROCS to 4

--- am.sh
+++ am.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-~/go/src/github.com/prometheus/alertmanager/alertmanager -config.file=prom/alertmanager.yml -storage.path=./am-data -web.listen-address=127.0.0.1:9093 -web.external-url=http://127.0.0.1:9093
+export GOMAXPROCS=4
+~/go/src/github.com/prometheus/alertmanager/alertmanager \
+    -config.file=prom/alertmanager.yml \
+    -storage.path=./am-data \
+    -web.listen-address=127.0.0.1:9093 \
+    -web.external-url=http://127.0.0.1:9093 \
+    -log.level=debug
--- prom.sh
+++ prom.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-#rm -rf data # Default local storage.
-~/go/src/github.com/prometheus/prometheus/prometheus -config.file=prom/prometheus.yml -alertmanager.url=http://127.0.0.1:9093 -storage.local.path=./prom-data -web.external-url=http://127.0.0.1:9090
+export GOMAXPROCS=4
+~/go/src/github.com/prometheus/prometheus/prometheus \
+    -config.file=prom/prometheus.yml \
+    -alertmanager.url=http://127.0.0.1:9093 \
+    -storage.local.path=./prom-data \
+    -web.external-url=http://127.0.0.1:9090 \
+    -web.listen-address=127.0.0.1:9090 \
+    -log.level=debug
