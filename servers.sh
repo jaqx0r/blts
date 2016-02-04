ffa04c5684a62946c829c493847ba72177c27439
Jamie Wilkinson <jaq@spacepants.org>
2016-02-04 16:31:01 +1100

    comment

--- servers.sh
+++ servers.sh
@@ -10,6 +10,7 @@ renice +10 $$
 
 BACKENDS=""
 for i in $(seq 0 9); do
+    # fire up some old-school containers
     (s/s --port 800$i 2>&1 | sed -e "s/^/s:800$i: /") &
     BACKENDS="$BACKENDS :800$i"
 done
