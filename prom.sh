1749dd808fa585edd89e2faebf1ad472d6d1984f
Jamie Wilkinson <jaq@spacepants.org>
2018-06-04 06:49:42 +1000

    Rewrite rules to 2.x format.

--- prom.sh
+++ prom.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 export GOMAXPROCS=4
-~/go/src/github.com/prometheus/prometheus/prometheus \
-    -config.file=prom/prometheus.yml \
-    -alertmanager.url=http://127.0.0.1:9093 \
-    -storage.local.path=./prom-data \
-    -web.external-url=http://127.0.0.1:9090 \
-    -web.listen-address=127.0.0.1:9090 \
-    -log.level=debug
+~/src/blts/prometheus-2.2.1.linux-amd64/prometheus \
+    --config.file=prom/prometheus.yml \
+    --web.external-url=http://127.0.0.1:9090 \
+    --web.listen-address=127.0.0.1:9090 \
+    --log.level=debug
