f031ab1cff9a6eb368e4d9580ebb4cc8f4a87a92
Jamie Wilkinson <jaq@spacepants.org>
2025-03-14 18:06:54 +1100

    feat: The demo now loads and runs an image.

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"os"
+	"path/filepath"
+	"io"
 
 	"github.com/bazelbuild/rules_go/go/runfiles"
-	"github.com/testcontainers/testcontainers-go"
+	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/layout"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/testcontainers/testcontainers-go"
 )
 
 type DemoContainers struct {
@@ -15,41 +19,99 @@ type DemoContainers struct {
 	loadBalander testcontainers.Container
 }
 
-func SetupContainers(ctx context.Context) (*DemoContainers, error) {
-	imagePath, err := runfiles.Rlocation(filepath.Join("blts", "/cmd/s/image"))
+func loadImage(ctx context.Context, client *testcontainers.DockerClient, imagePath, imageName string) error {
+	fullPath, err := runfiles.Rlocation(filepath.Join("blts", imagePath))
 	if err != nil {
-		return nil, err
+		return nil
 	}
-	path, err := layout.FromPath(imagePath)
-	if err != nil {
-		return nil, err
-	}
-	img, err := path.ImageIndex()
-	if err != nil {
-		return nil, err
-	}
-	manifest, err := img.IndexManifest()
-	if err != nil {
-		return nil, err
-	}
-	for _, m := range manifest.Manifests() {
-		fmt.Printf("manifgest digest: %v", m.Digest())
-	}
-	// // client, err := testcontainers.NewDockerClientWithOpts(ctx)
-	// // if err != nil {
-	// // 	return nil, err
-	// // }
-	// // reader, err := os.Open(imagePath)
-	// // if err != nil {
-	// // 	return nil, err
-	// // }
-	// // resp, err := client.ImageLoad(ctx, reader, false)
-	// // if err != nil {
-	// // 	return nil, err
-	// // }
-	// // defer resp.Body.Close()
-	return &DemoContainers{}, nil
 
+	path, err := layout.FromPath(fullPath)
+	if err != nil {
+		return err
+	}
+
+	index, err := path.ImageIndex()
+	if err != nil {
+		return err
+	}
+
+	manifest, err := index.IndexManifest()
+	if err != nil {
+		return err
+	}
+
+	for _, manifestDescriptor := range manifest.Manifests {
+		hash := manifestDescriptor.Digest
+
+		img, err := path.Image(hash)
+		if err != nil {
+			return err
+		}
+
+		ref, err := name.ParseReference(imageName)
+		if err != nil {
+			return err
+		}
+		pr, pw := io.Pipe()
+		go func() {
+			pw.CloseWithError(tarball.Write(ref, img, pw))
+		}()
+
+		notQuiet := false
+		resp, err := client.ImageLoad(ctx, pr, notQuiet)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		resp.Body.Close()
+	}
+	return nil
+}
+
+func SetupContainers(ctx context.Context) (*DemoContainers, error) {
+	client, err := testcontainers.NewDockerClientWithOpts(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = loadImage(ctx, client, "cmd/s/image", "hiserver")
+	if err != nil {
+		return nil, err
+	}
+
+	req := testcontainers.ContainerRequest{
+		Image:        "hiserver:latest",
+		ExposedPorts: []string{"8000/tcp"},
+	}
+	hiserver, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	// TODO: hiserver.TerminateContainer
+
+	info, err := hiserver.Inspect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("container info: %s\n", info.Config.Image)
+	endpoint, err := hiserver.PortEndpoint(ctx, "8000", "http")
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("endpoint %s\n", endpoint)
+
+	fmt.Scanln()
+
+	return nil, nil
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	contrib.go.opencensus.io/exporter/prometheus v0.4.2
 	contrib.go.opencensus.io/exporter/zipkin v0.1.2
 	github.com/bazelbuild/rules_go v0.53.0
+	github.com/docker/docker v27.5.0+incompatible
 	github.com/google/go-containerregistry v0.20.3
 	github.com/openzipkin/zipkin-go v0.4.3
 	github.com/prometheus/client_golang v1.21.1
@@ -29,7 +30,6 @@ require (
 	github.com/cpuguy83/dockercfg v0.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
-	github.com/docker/docker v27.5.0+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
